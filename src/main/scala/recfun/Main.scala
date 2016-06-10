bbe2966463ed857e314558b01894c6f9f699b3f6
Vincenzo D'Amore <v.damore@gmail.com>
2016-06-10 15:57:09 +0200

    removed warnings on multiple var

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -18,27 +18,26 @@ object Main {
           pascal(c - 1, r - 1) + pascal(c, r - 1)
         else
           1
-    }                                             //> pascal: (c: Int, r: Int)Int
+    }
   
   /**
    * Exercise 2
    */
     def balance(chars: List[Char]): Boolean = {
       def loop(chars: List[Char], acc: Int): Int = {
-        var c = acc
         if (!chars.isEmpty) {
           val head = chars.head
           if (head == '(') {
-            c = loop(chars.tail, c + 1)
+            loop(chars.tail, acc + 1)
           } else if (head == ')') {
             if (acc > 0)
-              c = loop(chars.tail, c - 1)
+              loop(chars.tail, acc - 1)
             else 
-              c = -1
+              -1
           } else
-            c = loop(chars.tail, c)
-        }
-        c
+            loop(chars.tail, acc)
+        } else
+          acc
       }
       loop(chars, 0) == 0
     }
@@ -49,35 +48,32 @@ object Main {
     def countChange(money: Int, coins: List[Int]): Int = {
         
       def countCoins(s: List[Int], rest: Int, counter: Int): Int = {
-        var c = counter
         if (!s.isEmpty) {
-          var curRest = rest - s.head
+          val curRest = rest - s.head
           if (!(curRest < 0)) {
             if (curRest >= s.head) {
               if (s.size > 1) {
-                c = countCoins(s.tail, curRest, c)
-              }
-              c = countCoins(s, curRest, c)
+                countCoins(s, curRest, countCoins(s.tail, curRest, counter) )
+              } else
+                countCoins(s, curRest, counter)
             } else if (curRest == 0 && s.size == 1) {
-              c = c + 1
+               counter + 1
             } else
-                c = countCoins(s.tail, curRest, c)
-          }
-        }
-        c
+               countCoins(s.tail, curRest, counter)
+          } else
+            counter
+        } else
+          counter
       }
   
       def getCombinations(s: List[Int]): Int = { innerGetCombinations(List(), s, 0) }
   
       def innerGetCombinations(prefix: List[Int], s: List[Int], counter : Int): Int = {
-        var c = counter
         if (s.size > 0) {
           val curComb = prefix :+ s.head
-          c = countCoins(curComb, money, c)
-          c = innerGetCombinations(prefix :+ s.head, s.tail, c);
-          c = innerGetCombinations(prefix, s.tail, c);
-        }
-        c
+          innerGetCombinations(prefix, s.tail, innerGetCombinations(prefix :+ s.head, s.tail, countCoins(curComb, money, counter)));
+        } else
+          counter
       }
   
       val s = coins.sorted.reverse
--- src/test/scala/recfun/BalanceSuite.scala
+++ src/test/scala/recfun/BalanceSuite.scala
@@ -10,20 +10,24 @@ import org.scalatest.junit.JUnitRunner
 class BalanceSuite extends FunSuite {
   import Main.balance
 
-  test("balance: '(if (zero? x) max (/ 1 x))' is balanced") {
-    assert(balance("(if (zero? x) max (/ 1 x))".toList))
-  }
-
-  test("balance: 'I told him ...' is balanced") {
-    assert(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
+  test("balance: counting is not enough") {
+    assert(!balance("())(".toList))
   }
 
   test("balance: ':-)' is unbalanced") {
     assert(!balance(":-)".toList))
   }
 
-  test("balance: counting is not enough") {
-    assert(!balance("())(".toList))
+
+  test("(((hello)there)you)(") {
+    assert(!balance("(a)(".toList))
+  }
+  test("balance: '(if (zero? x) max (/ 1 x))' is balanced") {
+    assert(balance("(if (zero? x) max (/ 1 x))".toList))
+  }
+
+  test("balance: 'I told him ...' is balanced") {
+    assert(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
   }
 
 }
