fa8c03fc61911151cbb8ce51219c733104879347
Vincenzo D'Amore <v.damore@gmail.com>
2016-09-26 13:52:30 +0200

    first tests with pattern matching version

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -24,8 +26,11 @@ object Main {
    * Exercise 2
    */
     def balance(chars: List[Char]): Boolean = {
+      @tailrec
       def loop(chars: List[Char], acc: Int): Int = {
-        if (!chars.isEmpty) {
+        if (chars.isEmpty) {
+          acc
+        } else {
           val head = chars.head
           if (head == '(') {
             loop(chars.tail, acc + 1)
@@ -36,8 +41,7 @@ object Main {
               -1
           } else
             loop(chars.tail, acc)
-        } else
-          acc
+        }
       }
       loop(chars, 0) == 0
     }
@@ -47,36 +51,37 @@ object Main {
    */
     def countChange(money: Int, coins: List[Int]): Int = {
         
+//      @tailrec
       def countCoins(s: List[Int], rest: Int, counter: Int): Int = {
-        if (!s.isEmpty) {
-          val curRest = rest - s.head
-          if (!(curRest < 0)) {
-            if (curRest >= s.head) {
-              if (s.size > 1) {
-                countCoins(s, curRest, countCoins(s.tail, curRest, counter) )
+        s match {
+          case Nil => counter
+          case xs :: xt => {
+            val curRest = rest - s.head
+            if (!(curRest < 0)) {
+              if (curRest >= xs) {
+                if (s.size > 1) {
+                  countCoins(s, curRest, countCoins(xt, curRest, counter) )
+                } else
+                  countCoins(s, curRest, counter)
+              } else if (curRest == 0 && s.size == 1) {
+                 counter + 1
               } else
-                countCoins(s, curRest, counter)
-            } else if (curRest == 0 && s.size == 1) {
-               counter + 1
+                 countCoins(xt, curRest, counter)
             } else
-               countCoins(s.tail, curRest, counter)
-          } else
-            counter
-        } else
-          counter
+              counter
+          }
+        }
       }
   
-      def getCombinations(s: List[Int]): Int = { innerGetCombinations(List(), s, 0) }
-  
-      def innerGetCombinations(prefix: List[Int], s: List[Int], counter : Int): Int = {
+      def getCombinations(prefix: List[Int], s: List[Int], counter : Int): Int = {
         if (s.size > 0) {
           val curComb = prefix :+ s.head
-          innerGetCombinations(prefix, s.tail, innerGetCombinations(prefix :+ s.head, s.tail, countCoins(curComb, money, counter)));
+          getCombinations(prefix, s.tail, getCombinations(prefix :+ s.head, s.tail, countCoins(curComb, money, counter)));
         } else
           counter
       }
   
       val s = coins.sorted.reverse
-      getCombinations(s)
+      getCombinations(List(), s, 0)
     }
   }
